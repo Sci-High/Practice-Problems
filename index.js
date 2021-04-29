61965b5916f6a3315746eeda867f0358667c4052
Jonathan Corley <>
2021-04-29 17:02:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+/*
+Create a function triangular that takes 
+in the number and returns the sum of it 
+and all the numbers below it.
+
+*/
+
+function triangular(number){
+  //CODE BELOW HERE
+
+
+
+
+  //CODE ABOVE HERE
+}
+
+
+
+
+
+
+/*
+Create a function reverseString that 
+takes in a string and returns the 
+reversed version.
+*/
+
+function reverseString(string){
+  //CODE BELOW HERE
+
+
+
+
+  //CODE ABOVE HERE
+}
+
+
+
+
+
+
+/*
+Create a function timeConverter that 
+takes in a number representing minutes 
+and should return it converted to hours + 
+minutes formatted as: hours:minutes.
+*/
+
+function timeConverter(string){
+  //CODE BELOW HERE
+
+
+
+
+  //CODE ABOVE HERE
+}
